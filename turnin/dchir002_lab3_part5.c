43978e4b6dafd90f2fb934e3d8d0aeb92ec236f9
dit555 <dchir002@ucr.edu>
2020-04-15 23:30:40 -0700

    5P

---
+++ turnin/dchir002_lab3_part5.c
@@ -0,0 +1,39 @@
+/*	Author: Dumitru  lab 3
+ *  Partner(s) Name: 
+ *	Lab Section: 026
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+
+    DDRB = 0x00; PORTB = 0x00;
+    DDRD = 0x00; PORTD = 0x00;
+    unsigned char b = 0x00;
+    unsigned char d = 0x00;
+    while (1) {
+	d = PIND;
+	b = 0x00;
+
+	if (d >= 70)
+		b = b | 0x01;
+	
+	else if (d > 5 && d < 70){
+		b = b & 0x01;
+		b = b | 0x04;
+	}
+	else
+		b = 0x00;
+
+	PORTB = b;
+
+    }
+    return 1;
+}
